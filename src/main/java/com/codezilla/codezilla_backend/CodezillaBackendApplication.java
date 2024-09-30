9099d2cce0ca9d4ddba305d8345c564c5c400a18
Oshanik <64382839+Oshanik22@users.noreply.github.com>
2024-10-01 00:18:10 +0530

    first commit

---
+++ src/main/java/com/codezilla/codezilla_backend/CodezillaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codezilla.codezilla_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodezillaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodezillaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codezilla/codezilla_backend/CodezillaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codezilla.codezilla_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodezillaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
