d399316b039594a24cea0109c94ecd47ae468c52
Oshanik <64382839+Oshanik22@users.noreply.github.com>
2024-10-06 15:00:51 +0530

    create hell yeah apis

--- src/main/java/com/codezilla/codezilla_backend/HellYeah.java
+++ src/main/java/com/codezilla/codezilla_backend/HellYeah.java
@@ -1,2 +1,18 @@
-package com.codezilla.codezilla_backend;public class HellYeah {
+package com.codezilla.codezilla_backend;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellYeah {
+
+    @GetMapping("/core/hellyeah")
+    public String sayHellYeah() {
+        return "hell yeah";
+    }
+
+    @GetMapping("/core/helloworld")
+    public String sayHelloWorld() {
+        return "hello world";
+    }
 }
+
